ba1615025ad7d44db42b28093d19162f39e387cc
Cory Imel <cory.imel@gmail.com>
2018-01-29 10:16:31 -0600

    started deployment setup

--- src/main/java/host/caddy/CaddyApplication.java
+++ src/main/java/host/caddy/CaddyApplication.java
@@ -3,13 +3,18 @@ package host.caddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CaddyApplication {
+public class CaddyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(CaddyApplication.class, args);
 	}
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CaddyApplication.class);
+    }
 
 }
--- src/main/java/host/caddy/services/YelpSearch.java
+++ src/main/java/host/caddy/services/YelpSearch.java
@@ -44,10 +44,10 @@ public class YelpSearch {
             String req_payload = "{search(term: \"" + name + "\",latitude: " + lat + ", longitude: " + lon + ", limit: 1, radius: 1600){ business {name id rating url display_phone price photos location{address1 city state zip_code country formatted_address} coordinates {latitude longitude}}}}";
             HttpEntity<?> request = new HttpEntity<>(req_payload, headers);
             String url = "https://api.yelp.com/v3/graphql";
-
             try {
                 ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, request, String.class);
                 if (response.getStatusCode() == HttpStatus.OK) {
+
                     ObjectMapper objectMapper = new ObjectMapper();
                     yelp = objectMapper.readValue(response.getBody(), Yelp.class);
                     return response.getStatusCode().toString();
