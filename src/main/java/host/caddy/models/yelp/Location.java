7739fbde2d939f77b756f802ead95008d88d36c8
Cory Imel <cory.imel@gmail.com>
2018-01-28 18:12:35 -0600

    added yelp objects

---
+++ src/main/java/host/caddy/models/yelp/Location.java
@@ -0,0 +1,61 @@
+package host.caddy.models.yelp;
+
+public class Location {
+    private String address1;
+    private String city;
+    private String state;
+    private String zip_code;
+    private String country;
+    private String formatted_address;
+
+    public Location() {
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(String zip_code) {
+        this.zip_code = zip_code;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFormatted_address() {
+        return formatted_address;
+    }
+
+    public void setFormatted_address(String formatted_address) {
+        this.formatted_address = formatted_address;
+    }
+}
